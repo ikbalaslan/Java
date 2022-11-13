c13e3c07275f229def0925eef29d3fa45a83c435
Ikbal Aslan <ikball.aslann@gmail.com>
2022-11-14 00:16:50 +0300

    Upd-6

---
+++ CoreJava/CarRunner.java
@@ -0,0 +1,12 @@
+package day20accessmodifiersinheritance;
+
+public class CarRunner {
+    public static void main(String[] args) {
+
+        Civic obj1 = new Civic();
+        obj1.ecoEngine();
+
+
+
+    }
+}
