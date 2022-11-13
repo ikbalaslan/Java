a5ec22f3da167825d573309c385f5d907a37faa0
Ikbal Aslan <ikball.aslann@gmail.com>
2022-11-14 00:17:22 +0300

    Upd-8

---
+++ CoreJava/Civic.java
@@ -0,0 +1,10 @@
+package day20accessmodifiersinheritance;
+
+public class Civic extends Honda{
+    public void ecoEngine(){
+        System.out.println("Civic");
+    }
+public Civic(){
+    System.out.println("Civic class constructor 1");
+}
+}
