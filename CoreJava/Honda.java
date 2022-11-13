5ef4ae79cf258e26a08400cfd230d0262dbe7f67
Ikbal Aslan <ikball.aslann@gmail.com>
2022-11-14 00:17:46 +0300

    Upd-10

---
+++ CoreJava/Honda.java
@@ -0,0 +1,15 @@
+package day20accessmodifiersinheritance;
+
+public class Honda extends Car {
+
+
+    public void hondaMake() {
+        System.out.println("This car was made in Honda Factory");
+    }
+
+    public Honda() {
+        System.out.println("Honda class constructor 1");
+
+
+    }
+}
