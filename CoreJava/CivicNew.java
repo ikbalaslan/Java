2dff2aee9f8d23158da5f36db155fd13bdb409df
Ikbal Aslan <ikball.aslann@gmail.com>
2022-11-16 02:45:25 +0300

    Upd-16

---
+++ CoreJava/CivicNew.java
@@ -0,0 +1,15 @@
+package day21inheritance;
+
+public class Civic extends Honda {
+
+    public void ecoEngine(){
+        System.out.println("Civic");
+    }
+
+    public Civic(){
+        System.out.println("Civic class constructor 1");
+    }
+
+
+////Question-4-What is the result of   y<2 ? y <-1 ? z <-5 ? y <-1 ? z<5 ? 4 : 10 : -2 :  8 : 7  : 10
+}
