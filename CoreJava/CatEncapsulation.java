f203488f505ca8170a1d8d28257f2a48e5f7f329
Ikbal Aslan <ikball.aslann@gmail.com>
2022-11-17 00:55:45 +0300

    Upd-25

---
+++ CoreJava/CatEncapsulation.java
@@ -0,0 +1,25 @@
+package day23encapsulationabstraction;
+
+public class Cat extends Mammal{
+    public void meow(){
+        System.out.println("Cats meow...");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Cats eat...");
+
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("Cats are drink...");
+
+    }
+
+
+
+
+
+
+}
