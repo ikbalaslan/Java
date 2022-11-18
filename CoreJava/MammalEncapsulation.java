8c4dbabad16e6fcd11ccc886406b7d51db0269a5
Ikbal Aslan <ikball.aslann@gmail.com>
2022-11-18 20:37:01 +0300

    Upd-26

---
+++ CoreJava/MammalEncapsulation.java
@@ -0,0 +1,17 @@
+package day23encapsulationabstraction;
+
+public abstract class Mammal extends Animal{
+    /*
+    Normally child class ust override all "abstract methods" from the parent class.
+    But Mammal Class did not override anything from the Animal Class, no complain.
+
+Because "Overriding abstract methods from parent" is "Concrete Classes"
+"Overriding abstract methods from parent" is NOT mandatory for abstract classes.
+
+If you want you can override abstract methods from parent Animal to Mammal, it is optional.
+
+
+     */
+
+
+}
