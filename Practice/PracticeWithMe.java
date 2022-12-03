83082765ccb555695d0f688df8984607f5323a87
Ikbal Aslan <ikball.aslann@gmail.com>
2022-12-03 20:00:16 +0300

    Upd-13

---
+++ Practice/PracticeWithMe.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.*;
+
+public class PracticeWithMe {
+    public static void main(String[] args) {
+
+        int ar[] = {1, 2, 3, 4};
+
+        int x = 0;
+
+        try {
+
+            x = ar[ar.length-2];
+
+
+
+        }finally{
+
+            System.out.println("The Best");
+
+        }
+
+    }
+
+}
